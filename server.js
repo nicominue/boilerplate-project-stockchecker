1431e5de5c1e03ded0e3abfe528e51ec59333dab
nicominue <nicominue@gmail.com>
2025-12-08 23:01:01 -0300

    adding mongo db connection

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 'use strict';
 require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB conectado"))
+.catch(err => console.error("Error conectado a MongoDB:", err));
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
